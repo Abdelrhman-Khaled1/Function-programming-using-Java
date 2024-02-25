fc723eb7e01a55f8dda3b870e88bed5513667415
Abdelrhman <kabdo17013@gmail.com>
2024-02-25 13:53:57 +0200

    use optionals with functional programming

---
+++ src/main/java/Optionals/Main.java
@@ -0,0 +1,18 @@
+package Optionals;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Optional.ofNullable("abdo@gmail.com")
+                .ifPresentOrElse(
+                        value -> System.out.println("sending email to " + value),
+                        () -> System.out.println("Cannot send email ")
+                );
+
+//        Optional.ofNullable("ali@gmail.com")
+//                .ifPresent(System.out::println);
+
+    }
+}
